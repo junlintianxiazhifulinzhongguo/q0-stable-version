ea1f4c5fea525cf3dd7bbe93f1e6a3ee317ec4e0
root <root@localhost.localdomain>
2019-02-09 08:11:04 -0500

    修改了登录主页面第三方登录的代码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -117,10 +117,10 @@ export default {
     }
   },
   created() {
-    // window.addEventListener('hashchange', this.afterQRScan)
+    window.addEventListener('hashchange', this.afterQRScan)
   },
   destroyed() {
-    // window.removeEventListener('hashchange', this.afterQRScan)
+    window.removeEventListener('hashchange', this.afterQRScan)
   },
   methods: {
     showPwd() {
@@ -147,22 +147,18 @@ export default {
       })
     },
     afterQRScan() {
-      // const hash = window.location.hash.slice(1)
-      // const hashObj = getQueryObject(hash)
-      // const originUrl = window.location.origin
-      // history.replaceState({}, '', originUrl)
-      // const codeMap = {
-      //   wechat: 'code',
-      //   tencent: 'code'
-      // }
-      // const codeName = hashObj[codeMap[this.auth_type]]
-      // if (!codeName) {
-      //   alert('第三方登录失败')
-      // } else {
-      //   this.$store.dispatch('LoginByThirdparty', codeName).then(() => {
-      //     this.$router.push({ path: '/' })
-      //   })
-      // }
+      const hash = window.location.hash.slice(1).split("?")[1]
+      const hashObj = getQueryObject(hash)
+      const originUrl = window.location.origin
+      history.replaceState({}, '', originUrl)
+      const codeName = hashObj.code
+      if (!codeName) {
+        alert('第三方登录失败')
+      } else {
+        this.$store.dispatch('LoginByThirdparty', hashObj).then(() => {
+          this.$router.push({ path: '/' })
+        })
+      }
     }
   }
 }
